6824c6a60d9834bfff54739f3c499a522dcc3ba1
Alexandra Dernova <alexder@gmx.net>
2024-02-27 15:04:27 +0100

    Add you-are-not-logged-in Page

--- frontend/src/ProtectedRoutes.tsx
+++ frontend/src/ProtectedRoutes.tsx
@@ -1,5 +1,6 @@
-import {Navigate, Outlet} from "react-router-dom";
+import {/*Navigate,*/ Outlet} from "react-router-dom";
 import {MyUsersDto} from "./types/MyUsersDto.tsx";
+import PleaseLogIn from "./components/you-are-not-logged-in-page.tsx";
 
 
 type Props = {
@@ -10,6 +11,6 @@ export default function ProtectedRoutes(props: Props) {
     const isAuthenticated = props.user !== undefined && props.user !== null
 
     return (
-        isAuthenticated ? <Outlet /> : <Navigate to="/" />
+        isAuthenticated ? <Outlet /> : <PleaseLogIn/>
     )
 }
