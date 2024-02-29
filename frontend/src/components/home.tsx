8390ef49aee0871c7e2c8ab23d9dc0d13a38b226
Alexandra Dernova <alexder@gmx.net>
2024-02-29 19:51:29 +0100

    styling von map

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -11,17 +11,28 @@ export default function Home(props: Readonly<Props>) {
 
     return (
         <StyledDiv>
-            <h3>Willkommen auf unserer Webseite für die Planung, Speicherung und Bearbeitung von Reisen!</h3>
-            <p><i>
+            <StyledH3>Deine Reise, von der Planung bis zur Erinnerung: Alles in einer App!</StyledH3>
+            <StyledP><i>
                 Hier kannst du deine Abenteuer noch besser organisieren und deine Lieblingsrouten mit
                 Leichtigkeit erkunden. Hier kannst du nicht nur Routen planen und speichern, sondern auch
                 zusätzliche Informationen und Bilder zu jeder Route hinzufügen. Egal, ob du alleine unterwegs bist oder
                 deine Abenteuer mit Freunden teilst, wir helfen dir dabei, deine Erlebnisse zu dokumentieren.
-            </i></p>
+            </i></StyledP>
             <InteractiveMap routesData={props.routesData} planOn={false} isHome={true}/>
         </StyledDiv>
     )
 }
+
+const StyledP = styled.p`
+    margin: 0 10px 20px 10px;
+    text-align: center`;
+const StyledH3 = styled.h3`
+    color: #1c859c;
+    text-align: center;
+    font: small-caps bold 24px/1 sans-serif`;
 const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     padding: 2vw
 `;
